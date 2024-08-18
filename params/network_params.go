dfe62dedb6b6fabd86f55385dc3c49ad8b6027cf
Leeren Chang <leeren@leerenchang.com>
2024-08-18 00:57:25 -0700

    params/network: change B

--- params/network_params.go
+++ params/network_params.go
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// Some change for 0.9.2
 package params
 
 // These are network parameters that need to be constant between clients, but
